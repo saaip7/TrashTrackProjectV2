471e97a27834e2e1b51357f09fe48043efd10b3a
rilsaaip <syaifullahhilmi7@gmail.com>
2023-12-15 08:26:50 +0700

    Add project files.

---
+++ TrashTrackProjectV2/LoginPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TrashTrackProjectV2
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+            {
+                textEmail.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textEmail.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+            {
+                if (!string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0)
+                {
+                    if (txtEmail.Text == "admin" && txtPassword.Password == "admin")
+                    {
+                        MainWindow main = new MainWindow();
+                        main.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Email or Password is incorrect");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter your password");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter your email");
+            }   
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SignUpPage signUp = new SignUpPage();
+            signUp.Show();
+            this.Close();
+        }
+    }
+}
---
+++ TrashTrackProjectV2/SignUpPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TrashTrackProjectV2
+{
+    /// <summary>
+    /// Interaction logic for SignUpPage.xaml
+    /// </summary>
+    public partial class SignUpPage : Window
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoginPage loginPage = new LoginPage();
+            loginPage.Show();
+            this.Close();
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void textUsername_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void txtUsername_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textNama_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void txtNama_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textNomorTelp_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void txtNoTelp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textAlamat_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void txtAlamat_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
