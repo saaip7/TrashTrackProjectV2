5dfd2908709550b4650ced4059c104cd1106a64c
rilsaaip <syaifullahhilmi7@gmail.com>
2023-12-15 18:29:42 +0700

    update separated styles

---
+++ TrashTrackProjectV2/Utilities/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TrashTrackProjectV2.Utilities
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
