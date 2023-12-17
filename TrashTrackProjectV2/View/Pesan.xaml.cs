d712e5b2cc6acfe9238fb0d720a26109002c614d
rhnathar <zildan.athar@gmail.com>
2023-12-17 19:37:33 +0700

    add map

--- TrashTrackProjectV2/View/Pesan.xaml.cs
+++ TrashTrackProjectV2/View/Pesan.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Mapsui.UI.Wpf;
 using Mapsui;
+using Mapsui.Styles;
+using System.IO;
+using Mapsui.Layers;
 
 
 namespace TrashTrackProjectV2.View
@@ -31,6 +34,31 @@ namespace TrashTrackProjectV2.View
             //Map Borders
             MapControl.Map.Navigator.OverridePanBounds = new MRect(12250759.8997, -838054.2427, 12339231.2208, -924691.7367);
             MapControl.Map.Navigator.OverrideZoomBounds = new MMinMax(0, 200);
+
+            // Membuat Pin lokasi TPA
+            var pointFeature = new PointFeature(12250759.8997, -838054.2427)
+            {
+                Styles = new[] { new SymbolStyle { BitmapId = GetBitmapIdForEmbeddedResource("img/PinMap.png"), SymbolScale = 0.03 } },
+            };
+
+            // Menambahkan PointFeature ke layer pada peta
+            var layer = new MemoryLayer();
+            layer.Features = new List<PointFeature> { pointFeature };
+            layer.Style = null;
+            MapControl.Map.Layers.Add(layer);
+        }
+
+        //mendapat bitmap id dari gambar
+        private static int GetBitmapIdForEmbeddedResource(string imagePath)
+        {
+            using (FileStream fs = new FileStream(imagePath, FileMode.Open))
+            {
+                var memoryStream = new MemoryStream();
+                fs.CopyTo(memoryStream);
+                var bitmapId = BitmapRegistry.Instance.Register(memoryStream);
+                return bitmapId;
+            }
+
         }
     }
 }
